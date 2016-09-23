06e3d0a5eddd4c573e5114ac6152eac1f5022558
Cee <i@cee.moe>
2016-09-23 23:57:23 +0800

    [Close #3] Support Carthage

---
+++ PFWebViewControllerFramework/PFWebViewControllerFramework.h
@@ -0,0 +1,19 @@
+//
+//  PFWebViewControllerFramework.h
+//  PFWebViewControllerFramework
+//
+//  Created by Cee on 9/23/16.
+//  Copyright © 2016 Cee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PFWebViewControllerFramework.
+FOUNDATION_EXPORT double PFWebViewControllerFrameworkVersionNumber;
+
+//! Project version string for PFWebViewControllerFramework.
+FOUNDATION_EXPORT const unsigned char PFWebViewControllerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PFWebViewControllerFramework/PublicHeader.h>
+
+
